eab6be30889c7576eac4d0bfcac7a4e9d299f49f
你的名字 <>
2020-04-13 23:36:42 +0800

    第一天学习

--- page/Hello word/hello word.js
+++ page/Hello word/hello word.js
@@ -1,23 +1,27 @@
-// page/Hello word/hello word.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
+Page({
+  data:{
+    name:'Coderwhy',
+    age:18,
+    students:[
+      { id: 1, name: 'wujiax', age:22 },
+      { id: 2, name: 'nihao', age: 23 },
+      { id: 3, name: 'zhangsan', age: 24 },
+      { id: 4, name: 'lisi', age: 25 },
+    ],
+    counter:0,
   },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  handleBtnClick(){
+    //1.错误做法：界面是不会刷新的
+    // this.data.counter +=1,
+    //console.log(this.data.counter)
+    //2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
+    },
+    handleSubteraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
